fabb5b9799fffd0899390d7f050e6f447434db33
Matt Nuttall <mattnuttall00@gmail.com>
2021-07-14 20:48:02 +0100

    Code for each scenario to run in new projects

---
+++ R_projects/Scenario_4/Scen4_5reps.R
@@ -0,0 +1,422 @@
+#### SCENARIO 4 #####
+
+### As Scenario 3, but the wavelength is shorter (higher frequency) and the amplitude is smaller
+
+## Define manager budget
+s4 <- seq(0,50,1)
+MB4 <- 50*sin(2.5*s4+0)+500
+MB4 <- MB4[1:50]
+
+# Standardise so that cumulative total budget = 25,000
+MB4 <- 25000*(MB4/sum(MB4))
+
+
+
+### RUN 1
+UB  <- 400
+UBR <- 40
+
+MB <- MB4[1]
+
+
+Scen4_sim_old <- gmse_apply(
+  res_mod = resource,
+  obs_mod = observation,
+  man_mod = manager,
+  use_mod = user,
+  get_res = "FUll",
+  time_max = 50,
+  land_dim_1 = 150,
+  land_dim_2 = 150, 
+  res_movement = 0, 
+  agent_view = 150, 
+  agent_move = 50, 
+  res_move_type = 0, 
+  res_death_type = 0, 
+  observe_type = 2, 
+  times_observe = 1, 
+  obs_move_type = 1, 
+  res_min_age = 0, 
+  res_move_obs = FALSE, 
+  plotting = FALSE, 
+  res_consume = 0.08, 
+  
+  # all genetic algorithm parameters left to default
+  
+  move_agents = TRUE, 
+  max_ages = 1000, 
+  minimum_cost = 10, 
+  user_budget = UB, 
+  manager_budget = MB, 
+  usr_budget_rng = UBR,  
+  manage_target = 1125000, 
+  RESOURCE_ini = 1125000, 
+  culling = TRUE, 
+  tend_crops = TRUE,
+  tend_crop_yld = 0.01, 
+  stakeholders = 20, 
+  land_ownership = TRUE, 
+  public_land = 0, 
+  manage_freq = 1, 
+  group_think = FALSE
+)
+
+# matrix for results
+Scen4 <- matrix(data=NA, nrow=50, ncol=7)
+
+# loop the simulation. 
+for(time_step in 1:50){
+  
+  sim_new <- gmse_apply(get_res = "Full", old_list = Scen4_sim_old, user_budget=UB, 
+                        usr_budget_rng = UBR, manager_budget = MB)
+  
+  Scen4[time_step, 1] <- time_step
+  Scen4[time_step, 2] <- sim_new$basic_output$resource_results[1]
+  Scen4[time_step, 3] <- sim_new$basic_output$observation_results[1]
+  Scen4[time_step, 4] <- sim_new$basic_output$manager_results[3]
+  Scen4[time_step, 5] <- sum(sim_new$basic_output$user_results[,3])
+  Scen4[time_step, 6] <- UB
+  Scen4[time_step, 7] <- MB
+  
+  Scen4_sim_old <- sim_new
+  UB <- UB + 4.0816
+  UBR <- UB/10
+  MB <- MB4[time_step]
+}
+
+colnames(Scen4) <- c("Time", "Trees", "Trees_est", "Cull_cost", "Cull_count", "User_budget", "Manager_budget")
+Scen4_1_summary <- data.frame(Scen4)
+
+rm(Scen4_sim_old)
+rm(Scen4)
+
+write.csv(Scen4_1_summary, file = "outputs/investment/scenarios/Scenario_4/Scen4_1_summary.csv")
+
+
+
+### RUN 2
+UB  <- 400
+UBR <- 40
+
+MB <- MB4[1]
+
+
+Scen4_sim_old <- gmse_apply(
+  res_mod = resource,
+  obs_mod = observation,
+  man_mod = manager,
+  use_mod = user,
+  get_res = "FUll",
+  time_max = 50,
+  land_dim_1 = 150,
+  land_dim_2 = 150, 
+  res_movement = 0, 
+  agent_view = 150, 
+  agent_move = 50, 
+  res_move_type = 0, 
+  res_death_type = 0, 
+  observe_type = 2, 
+  times_observe = 1, 
+  obs_move_type = 1, 
+  res_min_age = 0, 
+  res_move_obs = FALSE, 
+  plotting = FALSE, 
+  res_consume = 0.08, 
+  
+  # all genetic algorithm parameters left to default
+  
+  move_agents = TRUE, 
+  max_ages = 1000, 
+  minimum_cost = 10, 
+  user_budget = UB, 
+  manager_budget = MB, 
+  usr_budget_rng = UBR,  
+  manage_target = 1125000, 
+  RESOURCE_ini = 1125000, 
+  culling = TRUE, 
+  tend_crops = TRUE,
+  tend_crop_yld = 0.01, 
+  stakeholders = 20, 
+  land_ownership = TRUE, 
+  public_land = 0, 
+  manage_freq = 1, 
+  group_think = FALSE
+)
+
+# matrix for results
+Scen4 <- matrix(data=NA, nrow=50, ncol=7)
+
+# loop the simulation. 
+for(time_step in 1:50){
+  
+  sim_new <- gmse_apply(get_res = "Full", old_list = Scen4_sim_old, user_budget=UB, 
+                        usr_budget_rng = UBR, manager_budget = MB)
+  
+  Scen4[time_step, 1] <- time_step
+  Scen4[time_step, 2] <- sim_new$basic_output$resource_results[1]
+  Scen4[time_step, 3] <- sim_new$basic_output$observation_results[1]
+  Scen4[time_step, 4] <- sim_new$basic_output$manager_results[3]
+  Scen4[time_step, 5] <- sum(sim_new$basic_output$user_results[,3])
+  Scen4[time_step, 6] <- UB
+  Scen4[time_step, 7] <- MB
+  
+  Scen4_sim_old <- sim_new
+  UB <- UB + 4.0816
+  UBR <- UB/10
+  MB <- MB4[time_step]
+}
+
+colnames(Scen4) <- c("Time", "Trees", "Trees_est", "Cull_cost", "Cull_count", "User_budget", "Manager_budget")
+Scen4_2_summary <- data.frame(Scen4)
+
+rm(Scen4_sim_old)
+rm(Scen4)
+
+
+
+
+### RUN 3
+UB  <- 400
+UBR <- 40
+
+MB <- MB4[1]
+
+
+Scen4_sim_old <- gmse_apply(
+  res_mod = resource,
+  obs_mod = observation,
+  man_mod = manager,
+  use_mod = user,
+  get_res = "FUll",
+  time_max = 50,
+  land_dim_1 = 150,
+  land_dim_2 = 150, 
+  res_movement = 0, 
+  agent_view = 150, 
+  agent_move = 50, 
+  res_move_type = 0, 
+  res_death_type = 0, 
+  observe_type = 2, 
+  times_observe = 1, 
+  obs_move_type = 1, 
+  res_min_age = 0, 
+  res_move_obs = FALSE, 
+  plotting = FALSE, 
+  res_consume = 0.08, 
+  
+  # all genetic algorithm parameters left to default
+  
+  move_agents = TRUE, 
+  max_ages = 1000, 
+  minimum_cost = 10, 
+  user_budget = UB, 
+  manager_budget = MB, 
+  usr_budget_rng = UBR,  
+  manage_target = 1125000, 
+  RESOURCE_ini = 1125000, 
+  culling = TRUE, 
+  tend_crops = TRUE,
+  tend_crop_yld = 0.01, 
+  stakeholders = 20, 
+  land_ownership = TRUE, 
+  public_land = 0, 
+  manage_freq = 1, 
+  group_think = FALSE
+)
+
+# matrix for results
+Scen4 <- matrix(data=NA, nrow=50, ncol=7)
+
+# loop the simulation. 
+for(time_step in 1:50){
+  
+  sim_new <- gmse_apply(get_res = "Full", old_list = Scen4_sim_old, user_budget=UB, 
+                        usr_budget_rng = UBR, manager_budget = MB)
+  
+  Scen4[time_step, 1] <- time_step
+  Scen4[time_step, 2] <- sim_new$basic_output$resource_results[1]
+  Scen4[time_step, 3] <- sim_new$basic_output$observation_results[1]
+  Scen4[time_step, 4] <- sim_new$basic_output$manager_results[3]
+  Scen4[time_step, 5] <- sum(sim_new$basic_output$user_results[,3])
+  Scen4[time_step, 6] <- UB
+  Scen4[time_step, 7] <- MB
+  
+  Scen4_sim_old <- sim_new
+  UB <- UB + 4.0816
+  UBR <- UB/10
+  MB <- MB4[time_step]
+}
+
+colnames(Scen4) <- c("Time", "Trees", "Trees_est", "Cull_cost", "Cull_count", "User_budget", "Manager_budget")
+Scen4_3_summary <- data.frame(Scen4)
+
+rm(Scen4_sim_old)
+rm(Scen4)
+
+
+
+
+### RUN 4
+UB  <- 400
+UBR <- 40
+
+MB <- MB4[1]
+
+
+Scen4_sim_old <- gmse_apply(
+  res_mod = resource,
+  obs_mod = observation,
+  man_mod = manager,
+  use_mod = user,
+  get_res = "FUll",
+  time_max = 50,
+  land_dim_1 = 150,
+  land_dim_2 = 150, 
+  res_movement = 0, 
+  agent_view = 150, 
+  agent_move = 50, 
+  res_move_type = 0, 
+  res_death_type = 0, 
+  observe_type = 2, 
+  times_observe = 1, 
+  obs_move_type = 1, 
+  res_min_age = 0, 
+  res_move_obs = FALSE, 
+  plotting = FALSE, 
+  res_consume = 0.08, 
+  
+  # all genetic algorithm parameters left to default
+  
+  move_agents = TRUE, 
+  max_ages = 1000, 
+  minimum_cost = 10, 
+  user_budget = UB, 
+  manager_budget = MB, 
+  usr_budget_rng = UBR,  
+  manage_target = 1125000, 
+  RESOURCE_ini = 1125000, 
+  culling = TRUE, 
+  tend_crops = TRUE,
+  tend_crop_yld = 0.01, 
+  stakeholders = 20, 
+  land_ownership = TRUE, 
+  public_land = 0, 
+  manage_freq = 1, 
+  group_think = FALSE
+)
+
+# matrix for results
+Scen4 <- matrix(data=NA, nrow=50, ncol=7)
+
+# loop the simulation. 
+for(time_step in 1:50){
+  
+  sim_new <- gmse_apply(get_res = "Full", old_list = Scen4_sim_old, user_budget=UB, 
+                        usr_budget_rng = UBR, manager_budget = MB)
+  
+  Scen4[time_step, 1] <- time_step
+  Scen4[time_step, 2] <- sim_new$basic_output$resource_results[1]
+  Scen4[time_step, 3] <- sim_new$basic_output$observation_results[1]
+  Scen4[time_step, 4] <- sim_new$basic_output$manager_results[3]
+  Scen4[time_step, 5] <- sum(sim_new$basic_output$user_results[,3])
+  Scen4[time_step, 6] <- UB
+  Scen4[time_step, 7] <- MB
+  
+  Scen4_sim_old <- sim_new
+  UB <- UB + 4.0816
+  UBR <- UB/10
+  MB <- MB4[time_step]
+}
+
+colnames(Scen4) <- c("Time", "Trees", "Trees_est", "Cull_cost", "Cull_count", "User_budget", "Manager_budget")
+Scen4_4_summary <- data.frame(Scen4)
+
+rm(Scen4_sim_old)
+rm(Scen4)
+
+
+
+### RUN 5
+UB  <- 400
+UBR <- 40
+
+MB <- MB4[1]
+
+
+Scen4_sim_old <- gmse_apply(
+  res_mod = resource,
+  obs_mod = observation,
+  man_mod = manager,
+  use_mod = user,
+  get_res = "FUll",
+  time_max = 50,
+  land_dim_1 = 150,
+  land_dim_2 = 150, 
+  res_movement = 0, 
+  agent_view = 150, 
+  agent_move = 50, 
+  res_move_type = 0, 
+  res_death_type = 0, 
+  observe_type = 2, 
+  times_observe = 1, 
+  obs_move_type = 1, 
+  res_min_age = 0, 
+  res_move_obs = FALSE, 
+  plotting = FALSE, 
+  res_consume = 0.08, 
+  
+  # all genetic algorithm parameters left to default
+  
+  move_agents = TRUE, 
+  max_ages = 1000, 
+  minimum_cost = 10, 
+  user_budget = UB, 
+  manager_budget = MB, 
+  usr_budget_rng = UBR,  
+  manage_target = 1125000, 
+  RESOURCE_ini = 1125000, 
+  culling = TRUE, 
+  tend_crops = TRUE,
+  tend_crop_yld = 0.01, 
+  stakeholders = 20, 
+  land_ownership = TRUE, 
+  public_land = 0, 
+  manage_freq = 1, 
+  group_think = FALSE
+)
+
+# matrix for results
+Scen4 <- matrix(data=NA, nrow=50, ncol=7)
+
+# loop the simulation. 
+for(time_step in 1:50){
+  
+  sim_new <- gmse_apply(get_res = "Full", old_list = Scen4_sim_old, user_budget=UB, 
+                        usr_budget_rng = UBR, manager_budget = MB)
+  
+  Scen4[time_step, 1] <- time_step
+  Scen4[time_step, 2] <- sim_new$basic_output$resource_results[1]
+  Scen4[time_step, 3] <- sim_new$basic_output$observation_results[1]
+  Scen4[time_step, 4] <- sim_new$basic_output$manager_results[3]
+  Scen4[time_step, 5] <- sum(sim_new$basic_output$user_results[,3])
+  Scen4[time_step, 6] <- UB
+  Scen4[time_step, 7] <- MB
+  
+  Scen4_sim_old <- sim_new
+  UB <- UB + 4.0816
+  UBR <- UB/10
+  MB <- MB4[time_step]
+}
+
+colnames(Scen4) <- c("Time", "Trees", "Trees_est", "Cull_cost", "Cull_count", "User_budget", "Manager_budget")
+Scen4_5_summary <- data.frame(Scen4)
+
+rm(Scen4_sim_old)
+rm(Scen4)
+
+
+## Combine all summaries
+Scen4_all_summary <- rbind(Scen4_1_summary,Scen4_2_summary,Scen4_3_summary,Scen4_4_summary,Scen4_5_summary)
+
+
+write.csv(Scen4_all_summary, file = "outputs/investment/scenarios/Scen4_all_summary.csv")
